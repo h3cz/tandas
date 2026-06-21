b37217dc7c4054a8e3addef802bb5864462300dd
Faded <jrlop99@gmail.com>
2026-06-20 19:14:35 -0500

    Publish Pages-compatible Tandas build

---
+++ assets/skeleton-CtEw7nGq.js
@@ -0,0 +1,1 @@
+import{j as m}from"./vendor-query-c047i3QP.js";import{c as o}from"./index-mCHMkcip.js";function n({className:e,...t}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-muted",e),...t})}export{n as S};
